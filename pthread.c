e545bbabfa5ed13731004182cb03e69756fb5dbf
notepi <root@raspberrypi.(none)>
2016-04-22 17:42:34 +0800

    添加消息队列

--- pthread.c
+++ pthread.c
@@ -11,10 +11,10 @@
 void * NEW_Pthread(void *arg)
 {
 	int cuid;
-	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);		// 允许关闭线程
-	pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);		// 遇到取消点关闭线程
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);				// 允许关闭线程
+	pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);				// 遇到取消点关闭线程
 	printf("receive:%s\n", arg);
-	cuid = pthread_self();										// 获取线程ID
+	cuid = pthread_self();												// 获取线程ID
 	printf("uid is:%u\n", cuid);
 	sleep(2);
 	printf("message 1!\n");
@@ -30,12 +30,12 @@ int main()
 	int ret;
 	void *retval;
 	
-	pid = getpid();												// 获取进程ID
-	c_uid = pthread_self();										// 获取线程ID
+	pid = getpid();															// 获取进程ID
+	c_uid = pthread_self();													// 获取线程ID
 	printf("pid is:%u\n", pid);
 	printf("c_uid is:%u\n", c_uid);
 	
-	ret = pthread_create(&uid, NULL,  NEW_Pthread, "im new pthread!");
+	ret = pthread_create(&uid, NULL,  NEW_Pthread, "im new pthread!");		// 创建线程
 	if( 0 != ret){
 		printf("errno value: %d, it means: %s\n",ret, strerror(ret));
 		return 0;
